69ddcf69f6322455af04a5ca9245e5a12ff0665b
Weidong Xu <weidxu@microsoft.com>
2021-01-25 13:56:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample project for data on Azure via Azure Java Data SDK
---
+++ src/main/java/com/microsoft/azure/data/Main.java
@@ -0,0 +1,23 @@
+package com.microsoft.azure.data;
+
+import com.azure.core.util.logging.ClientLogger;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+
+public class Main {
+
+    private static final ClientLogger logger = new ClientLogger(Main.class);
+
+    public static void main(String args[]) {
+
+        BlobContainerClient containerClient = new BlobContainerClientBuilder()
+                .endpoint("sa2weidxu.blob.core.windows.net")
+                .containerName("container1")
+                .credential(new ManagedIdentityCredentialBuilder().build())
+                .buildClient();
+
+        long count = containerClient.listBlobs().stream().count();
+        logger.info("Number of blobs: {}", count);
+    }
+}
